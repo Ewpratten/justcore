208162f6933505bcf19bca21aed044991b602932
Evan Pratten <ewpratten@gmail.com>
2020-09-29 13:59:04 -0400

    Awards

--- src/main/java/ca/retrylife/mc/justcore/JustCore.java
+++ src/main/java/ca/retrylife/mc/justcore/JustCore.java
@@ -1,13 +1,44 @@
 package ca.retrylife.mc.justcore;
 
+import java.io.IOException;
+import java.net.URL;
+
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import ca.retrylife.mc.justcore.model.JustPlayer;
+import ca.retrylife.mc.justcore.render.capes.Cape;
+import ca.retrylife.mc.justcore.render.capes.CustomPlayerCapeRegistry;
 import net.fabricmc.api.ModInitializer;
 
 public class JustCore implements ModInitializer {
+	private Logger logger = LogManager.getLogger(getClass());
 
 	@Override
 	public void onInitialize() {
-		// TODO Auto-generated method stub
-		
+		logger.log(Level.INFO, "Starting JustCore");
+
+		// Give mod developers their capes
+		try {
+			registerModDeveloperCapes();
+		} catch (IOException e) {
+			logger.log(Level.WARN,
+					"Failed to award mod developers with their capes. Maybe the resource is not available?");
+		}
+
+		logger.log(Level.INFO, "JustCore Ready");
 	}
-    
+
+	/**
+	 * Registers custom capes for all mod developers
+	 */
+	private void registerModDeveloperCapes() throws IOException {
+		// Define the mod developer cape
+		Cape devCape = new Cape(new URL("https://ewpratten.keybase.pub/JustCore-dev-cape.png"));
+
+		// Add every user account
+		CustomPlayerCapeRegistry.getInstance().overwritePlayerCape(new JustPlayer("Xnor50"), devCape);
+	}
+
 }
